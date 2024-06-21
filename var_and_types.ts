73079c761de8739b59daaf64d054857b2e63f59f
Camilla Barros <camillaerbarros@gmail.com>
2024-06-21 07:29:27 -0300

    update var_and_types.ts from my local repository

---
+++ var_and_types.ts
@@ -0,0 +1,18 @@
+// declarando variáveis
+
+var x: number = 10;
+let a: string = "Camilla"; 
+const b: boolean = true; // constantes não podem mudar ou serem atribuídas novamente
+let ax: number | string = 10; // tem a possibilidade de declarar mais de um tipo
+
+let num: number = 1.78;
+let guy: undefined;
+let girl: object = {};
+
+// verificado o tipo de cada variável
+console.log(typeof x);
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof ax);
+console.log(typeof guy);
+console.log(typeof girl);
